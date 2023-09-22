b7c460c378090066caf186aa7ffdf3d8c4fe3b17
joyshaheb <khondokoralam@gmail.com>
2023-09-22 22:42:26 +0600

    backend removed -no need for it

--- server/src/Routes/productRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getAllProducts } from "../Controller/productController";
-
-export const productRouter = express.Router();
-
-productRouter.get("/get-all", getAllProducts);
